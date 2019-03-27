507806aac4d5e310135f68fd3cad90c809f5967f
OrayKT <oray.kt@gmail.com>
2019-03-27 18:01:36 +0100

    Project based express generator

--- app.js
+++ app.js
@@ -1,37 +1,45 @@
-const express = require('express');
-const findMultiple = require('./helper/findMultiple');
-const app = express();
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const bodyParser = require('body-parser');
 
-const availableNodes = [100, 50, 20, 10];
 
-app.use((req, res, next) => {
-    next();
+var requestRouter = require('./routes/requestRouter');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/request', requestRouter);
+
+app.get('/', (req, res, next) => {
+  res.render('index', { title: 'Express' });
+});
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
 });
 
-app.get('/request', (req, res, next) => {
-    try {
-        let userInput = req.query.amount;
-        const resultArr = [];
-        if (!userInput) {
-            res.json({ result: resultArr });
-        }
-        if (userInput < 0) throw "InvalidArgumentException";
-        availableNodes.forEach((node, index) => {
-            let tempDiv = findMultiple(index, userInput, availableNodes);
-            if (tempDiv) {
-                userInput -= tempDiv * node;
-                do {
-                    resultArr.push(node);
-                    tempDiv--;
-                } while (tempDiv !== 0)
-            }
-        });
-        res.json({ result: resultArr });
-    } catch (exception) {
-        res.json({ exception });
-    }
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
-app.listen(3000, () => {
-    console.log('App is listening 3000 of port');
-});
+module.exports = app;
---
+++ routes/requestRouter.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var router = express.Router();
+const findMultiple = require('../helper/findMultiple');
+
+const availableNodes = [100, 50, 20, 10];
+router.get('/', (req, res, next) => {
+  try {
+    let userInput = req.query.amount;
+    const resultArr = [];
+    if (!userInput) {
+      res.json({ result: resultArr });
+    }
+    if (userInput < 0) throw "InvalidArgumentException";
+    availableNodes.forEach((node, index) => {
+      let tempDiv = findMultiple(index, userInput, availableNodes);
+      if (tempDiv) {
+        userInput -= tempDiv * node;
+        do {
+          resultArr.push(node);
+          tempDiv--;
+        } while (tempDiv !== 0)
+      }
+    });
+    res.json({ result: resultArr });
+  } catch (exception) {
+    res.json({ exception });
+  }
+});
+
+router.post('/', (req, res, next) => {
+  try {
+    let userInput = req.body.amount;
+    const resultArr = [];
+    if (!userInput) {
+      res.json({ result: resultArr });
+    }
+    if (userInput < 0) throw "InvalidArgumentException";
+    availableNodes.forEach((node, index) => {
+      let tempDiv = findMultiple(index, userInput, availableNodes);
+      if (tempDiv) {
+        userInput -= tempDiv * node;
+        do {
+          resultArr.push(node);
+          tempDiv--;
+        } while (tempDiv !== 0)
+      }
+    });
+    res.json({ result: resultArr });
+  } catch (exception) {
+    res.json({ exception });
+  }
+});
+module.exports = router;
+
