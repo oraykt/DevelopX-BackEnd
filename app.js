5f46b6e343d9ac7b91360a123d6d5bf49923560d
OrayKT <oray.kt@gmail.com>
2019-03-27 15:37:14 +0100

    ejs

--- app.js
+++ app.js
@@ -1,43 +1,23 @@
 const express = require('express');
-const gcd = require('./helper/calculateGCD');
+const findMultiple = require('./helper/findMultiple');
 const app = express();
 
 const availableNodes = [100, 50, 20, 10];
 
-app.use((req, res, next) => {
-    /*
-    fs.appendFile("developX.log", "Data" + "\n", (err)=>{
-        if(err){
-            console.log(error);
-        }
-    });
-    */
-    //console.log(req);
-    next();
-});
+app.set('view engine', 'ejs');
 
 app.get('/request/:amount', (req, res, next) => {
     try {
         let userInput = req.params.amount;
-        console.log(userInput);
         const resultArr = [];
         if (!userInput) {
             return resultArr;
         }
         if (userInput < 0) throw "InvalidArgumentException";
         availableNodes.forEach((node, index) => {
-            let tempDiv;
-            if (index === availableNodes.length - 1) {
-                if (userInput % node !== 0) {
-                    throw "NoteUnavailableException";
-                } else {
-                    tempDiv = userInput / node;
-                }
-            } else {
-                tempDiv = Math.floor(userInput / node);
-            }
+            let tempDiv = findMultiple(index, userInput, availableNodes);
             if (tempDiv) {
-                userInput = userInput - tempDiv * node;
+                userInput -= tempDiv * node;
                 do {
                     resultArr.push(node);
                     tempDiv--;
@@ -48,43 +28,8 @@ app.get('/request/:amount', (req, res, next) => {
     } catch (exception) {
         res.send(exception);
     }
-
-
-
-    // for (let node of availableNodes) {
-    //     console.log(index);
-    //     let tempDiv = Math.floor(userInput / node);
-    //     if (tempDiv) {
-    //         userInput -= tempDiv * node;
-    //         do {
-    //             resultArr.push(node);
-    //             tempDiv--;
-    //         } while (tempDiv !== 0)
-    //     } else {
-    //         console.log(tempDiv);
-    //     }
-    // }
-
-
     res.send();
-    /*
-        Amount should be multiple [10] , [10,20] , [10,20,50] , [10,20,50,100]
-            if(multiple()){
-                return Result[];
-            }else{
-                return AmountUnavailableException
-            }
-        Amount should be above than 0
-            if(above){
-                check multiple(amount)
-                then calculate
-            }else{
-                return InvalidArgumentException
-            }
-        If The amount is null, return Empty Set
-    */
 });
-
 
 app.listen(3000, () => {
     console.log('App is listening 3000 of port');
--- helper/calculateGCD.js
+++
@@ -1,9 +0,0 @@
-const gcd = (array) => {
-    let total = 1;
-    for (let element of array) {
-        total *= element;
-    }
-    return total;
-}
-
-module.exports = gcd;
