4a288bf1875b62bb7ecfb04bacfc76cc921a08ac
oraykt <oraykt@gmail.com>
2019-03-28 17:30:22 +0100

    Eslint

--- routes/requestRouter.js
+++ routes/requestRouter.js
@@ -1,46 +1,45 @@
-const express = require('express');
-const router = express.Router();
-const findResult = require('../helper/findResult');
-const availableNotes = [100, 50, 20, 10];
+const express = require('express')
+const router = express.Router()
+const findResult = require('../helper/findResult')
+const availableNotes = [100, 50, 20, 10]
 
 router.get('/:amount', (req, res, next) => {
   try {
-    const resultArr = findResult(availableNotes, req.params.amount);
+    const resultArr = findResult(availableNotes, req.params.amount)
     res.render('result', {
       result: resultArr
-    });
+    })
   } catch (exception) {
     res.render('error', {
       exception
-    });
+    })
   }
-});
+})
 
 router.get('/', (req, res, next) => {
   try {
-    req.query.amount = req.query.amount === undefined ? 0 : req.query.amount;
-    const resultArr = findResult(availableNotes, req.query.amount);
+    req.query.amount = req.query.amount === undefined ? 0 : req.query.amount
+    const resultArr = findResult(availableNotes, req.query.amount)
     res.render('result', {
       result: resultArr
-    });
+    })
   } catch (exception) {
     res.render('error', {
       exception
-    });
+    })
   }
-});
+})
 
 router.post('/', (req, res, next) => {
   try {
-    const resultArr = findResult(availableNotes, req.body.amount);
+    const resultArr = findResult(availableNotes, req.body.amount)
     res.render('result', {
       result: resultArr
-    });
+    })
   } catch (exception) {
     res.render('error', {
       exception
-    });
+    })
   }
-});
-module.exports = router;
-
+})
+module.exports = router
