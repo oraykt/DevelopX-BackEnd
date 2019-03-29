6f669d99222076fc132c94abcc3e7b6b66ab5f58
oraykt <oraykt@gmail.com>
2019-03-29 20:06:36 +0100

    Empty Set

--- utils/checkUserInput.js
+++ utils/checkUserInput.js
@@ -1,10 +1,7 @@
 /* eslint-disable no-throw-literal */
 module.exports = (userInput, lastNote) => {
   if (!isNaN(userInput)) {
-    if (userInput === 0) {
-      throw 'Empty Set'
-    }
-    if (userInput > 0 && userInput <= 10000) {
+    if (userInput >= 0 && userInput <= 10000) {
       if (userInput % lastNote === 0) {
         return userInput
       } else {
