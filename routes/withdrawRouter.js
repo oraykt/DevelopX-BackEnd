f558f18808f2cefff1e8c3d6a9a740924608192b
oraykt <oraykt@gmail.com>
2019-03-30 15:42:27 +0100

    cashmachine/withdraw

--- routes/withdrawRouter.js
+++ routes/withdrawRouter.js
@@ -5,12 +5,8 @@ const withdrawService = require('../services/withdrawService')
 
 const availableNotes = [100, 50, 20, 10]
 
-router.get('/:amount', (req, res, next) => {
-  withdrawService(req.params.amount, availableNotes, res)
-})
-
 router.get('/', (req, res, next) => {
-  withdrawService(req.query.amount, availableNotes, res)
+  res.render('withdraw')
 })
 
 router.post('/', (req, res, next) => {
