0af2cdb47cea2d90b206896f22d2395836dc9851
oraykt <oraykt@gmail.com>
2019-03-30 16:32:52 +0100

    Calculate Amount implemented frontside

--- routes/withdrawRouter.js
+++ routes/withdrawRouter.js
@@ -7,11 +7,15 @@ const availableNotes = [100, 50, 20, 10]
 // TODO userInput name
 
 router.get('/', (req, res, next) => {
-  console.log(req.query.amount)
   res.render('withdraw')
+})
+
+router.post('/withFront', (req, res, next) => {
+  withdrawService.withdraw(req.body.amount, availableNotes, res)
 })
 
 router.post('/', (req, res, next) => {
   withdrawService.withdraw(req.body.amount, availableNotes, res)
 })
+
 module.exports = router
