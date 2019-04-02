a93301f84bd59bfdaf1c80b0f1a99a7c30a90e2c
oraykt <oraykt@gmail.com>
2019-04-02 18:29:32 +0200

    Test Req and Res compared

--- test/withdrawRouter.js
+++ test/withdrawRouter.js
@@ -6,10 +6,49 @@ const server = require('../app')
 
 chai.use(chaiHttp)
 
-const successArray = ['', 0, 10, 20, 30, 40, 50, 80, 100, 180]
-const failArray = [1, 9, 125, -130, 'string']
+const sucessRequestArray = ['', 0, 10, 20, 30, 40, 50, 80, 100, 180]
+const expectedSuccessResponseArray = [
+  { result: [] },
+  { result: [] },
+  { result: [10] },
+  { result: [20] },
+  { result: [20, 10] },
+  { result: [20, 20] },
+  { result: [50] },
+  { result: [50, 20, 10] },
+  { result: [100] },
+  { result: [100, 50, 20, 10] }
+]
+const failRequestArray = [1, 9, 125, -130, 'string']
+const expectedFailResponseArray = [
+  {
+    exception: {
+      error: 'NoteUnavailableException'
+    }
+  },
+  {
+    exception: {
+      error: 'NoteUnavailableException'
+    }
+  },
+  {
+    exception: {
+      error: 'NoteUnavailableException'
+    }
+  },
+  {
+    exception: {
+      error: 'InvalidArgumentException'
+    }
+  },
+  {
+    exception: {
+      error: 'InvalidArgumentException'
+    }
+  }
+]
 
-successArray.map((val, index) => {
+sucessRequestArray.map((val, index) => {
   describe(`POST /withdraw body: { amount: ${val}  }`, () => {
     it('It should return Array', (done) => {
       chai.request(server)
@@ -22,14 +61,15 @@ successArray.map((val, index) => {
           res.should.an('Object')
           res.body.should.have.property('result')
           res.body.should.have.property('result').an('array')
-          console.log(res.body.result)
+          res.body.result.should.be.eql(expectedSuccessResponseArray[index].result)
+          console.log('Result: ', res.body.result)
           done()
         })
     })
   })
 })
 
-failArray.map((val, index) => {
+failRequestArray.map((val, index) => {
   describe(`POST /withdraw body: { amount: ${val} }`, () => {
     it('It should throw `Exception` with statusCode: 400', (done) => {
       chai.request(server)
@@ -44,8 +84,9 @@ failArray.map((val, index) => {
           res.body.exception.should.an('Object')
           res.body.exception.should.have.property('error')
           res.body.exception.should.have.property('error_msg')
-          console.log(res.body.exception.error)
-          console.log(res.body.exception.error_msg)
+          res.body.exception.error.should.be.eql(expectedFailResponseArray[index].exception.error)
+          console.log('Exception: ', res.body.exception.error)
+          console.log('Exception Message : ', res.body.exception.error_msg)
           done()
         })
     })
